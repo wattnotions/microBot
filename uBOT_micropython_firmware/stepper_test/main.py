16b9a60e0aef05e0d9bf6e51c87c52e04eedde3e
wattnotions <shaneormonde@gmail.com>
2021-02-07 16:13:23 +0000

    added debug print statements

--- uBOT_micropython_firmware/stepper_test/main.py
+++ uBOT_micropython_firmware/stepper_test/main.py
@@ -5,7 +5,7 @@ import time
 
 print("IM IN MAIN")
 
-msdelay = 25
+msdelay = 1000
 
 la = 19
 lb = 21
@@ -15,7 +15,7 @@ ld = 23
 
 #A and B are the same coil
 left_a = Pin(la, Pin.OUT)    
-left_b = Pin(lb, Pin.OUT)    0
+left_b = Pin(lb, Pin.OUT)   
 
 #C and D are the same coil
 left_c = Pin(lc, Pin.OUT)    
@@ -30,27 +30,35 @@ def disable_steppers():
     print("Steppers disabled")
     
 
-
+run_time = 0
 start_time = time.time()
-While (run_time < 10):
+while (run_time < 10):
     run_time = time.time() - start_time
+    
+    print("1")
     left_a.value(1)
     left_b.value(0)
     time.sleep_ms(msdelay)
     
-    left_c.value(0)
-    left_d.value(1)
-    time.sleep_ms(msdelay)
-    
+    print("2")
     left_a.value(0)
     left_b.value(1)
     time.sleep_ms(msdelay)
     
+    
+    print("3")
     left_c.value(1)
     left_d.value(0)
     time.sleep_ms(msdelay)
     
-    print("loop2.0")
+    
+    print("4")
+    left_c.value(0)
+    left_d.value(1)
+    time.sleep_ms(msdelay)
+    
+    
+    print("loop6.0")
     
     
     
@@ -60,4 +68,6 @@ disable_steppers()
     
     
     
+
+
 
